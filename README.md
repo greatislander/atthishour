02ce64e782f023100317fd2056fc82d5141bb2b7
Ned Zimmerman <ned@bight.ca>
2017-11-16 19:10:50 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# At This Hour
+
+Who is to blame?
+
+*"Picture of Halifax City Council, 1903" via [Wikimedia Commons](https://commons.wikimedia.org/wiki/File:City_Council_and_Department_Heads,_Halifax,_Nova_Scotia,_Canada,_1903_-_cropped.jpg).*
---
+++ public/scripts/atthishour.js
@@ -0,0 +1,8 @@
+const elem = document.querySelector('.ticker');
+const opts = {
+  typeSpeed: 75,
+  loop: true
+};
+malarkey(elem, opts).type('The first games of the National Hockey League are played in Montreal and Ottawa /// ')
+  .type('Robert Borden’s new wartime elections act disenfranchises conscientious objectors /// ')
+  .type('Half-a-million women vote for the first time in Canada’s federal election /// ')
