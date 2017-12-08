d5ebfa6a792608b7a8dbb969f561672151537c9d
Ned Zimmerman <ned@bight.ca>
2017-12-08 10:36:49 -0400

    Add placeholder.

--- routes/placeholder.js
+++ routes/placeholder.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "At This Hour" });
+  res.render("placeholder", { title: "At This Hour" });
 });
 
 module.exports = router;
