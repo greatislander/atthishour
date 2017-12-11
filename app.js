e6cdda3a7b5d08cfb147cca0b7876e19c31332cb
Ned Zimmerman <ned@bight.ca>
2017-12-10 20:55:58 -0400

    Remove credits route for now.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
-app.use("/credits", credits);
+// app.use("/credits", credits);
 app.use("/data", data);
 
 // catch 404 and forward to error handler
