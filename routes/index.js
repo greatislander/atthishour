07b45ef35d6872461040141a878b23b6e86f779b
Ned Zimmerman <ned@bight.ca>
2017-12-20 09:53:24 -0400

    Set up redirect.

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  let commit = process.env.NOW_URL ? process.env.NOW_URL : Date.now();
-  commit = commit.replace("https://atthishour-", "").replace(".now.sh", "");
-  const showTime = process.env.SHOWTIME
-    ? process.env.SHOWTIME
-    : "2017-12-17T19:30Z";
-  res.render("index", {
-    title: "At This Hour",
-    showtime: showTime,
-    commit: commit,
-  });
+  res.redirect(
+    301,
+    "https://zuppatheatre.com/show/at-this-hour-the-deposition-of-harbour-pilot-francis-mackey/"
+  );
 });
 
 module.exports = router;
